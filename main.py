b27a6c424840398ff1542a4b69cc3611c64f815d
Brad Smith <bradmsmith@users.noreply.github.com>
2024-09-24 17:52:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,8 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+password = "This_Password_Is-Exposed!@$!@#$"
+
+
+
